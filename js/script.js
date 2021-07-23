22dfd84b6ede5f09c52afebd9229e626e9a8fae8
avatarfreak <avatarfreak83@gmail.com>
2021-07-23 20:13:55 +0530

    minor fixes

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@ menuBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
   menuToggleBtn.classList.toggle("active");
 });
+//Email validation
+const errorPlaceholder = document.querySelector(".error");
+const form = document.querySelector(".cta__form");
+
+//Error msg to be displayed on wrong email"Please enter a valid email")
+const error = (msg = "Please enter a valid email") => {
+  return (errorPlaceholder.textContent = msg);
+};
+
+//setError message
+const setError = (msg) => {
+  return error(msg);
+};
+
+//validate email
+const validateEmail = (email) => {
+  const validReg =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return email.match(validReg) ? true : false;
+};
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const email = evt.target.children[0].value;
+  validateEmail(email) ? setError("") : error();
+});
